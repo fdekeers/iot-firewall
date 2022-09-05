dc8afb3f7c242da50b169e065d15340634874631
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2022-09-05 11:06:58 +0200

    Started using CUnit

--- test/parsers/dns.c
+++ test/parsers/dns.c
@@ -1,24 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <CUnit/CUnit.h>
+// Custom libraries
 #include "packet-utils.h"
 #include "parsers/header.h"
 #include "parsers/dns.h"
+// CUnit
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
 
 
-int main(int argc, char **argv) {
+void test_dns() {
 
     char *hexstring = "450000912ecc40004011879dc0a80101c0a801a10035a6b5007d76b46dca8180000100020000000008627573696e6573730b736d61727463616d6572610361706902696f026d6903636f6d0000010001c00c0005000100000258002516636e616d652d6170702d636f6d2d616d7370726f78790177066d692d64756e03636f6d00c04000010001000000930004142f61e7";
     
     unsigned char *payload;
     size_t length = hexstr_to_payload(hexstring, &payload);
-
-    print_payload(length, payload);
+    CU_ASSERT_EQUAL(length, strlen(hexstring) / 2);
 
     payload = skip_headers(payload);
     dns_message message = dns_parse_message(length, payload);
     dns_print_message(message);
 
+}
+
+int main(int argc, char const *argv[])
+{
+    CU_initialize_registry();
+    CU_pSuite suite = CU_add_suite("dns", NULL, NULL);
+    CU_add_test(suite, "dns", test_dns);
+    CU_basic_run_tests();
     return 0;
 }
