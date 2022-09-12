28f3e77c3877ab0306bf41033855838ed6de8bcc
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2022-09-12 15:09:54 +0200

    DHCP parser ongoing + Added DHCP traces

--- include/parsers/dhcp.h
+++ include/parsers/dhcp.h
@@ -15,42 +15,123 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#define DHCP_HEADER_SIZE 236
+#define DHCP_MAX_OPTION_COUNT 20
+
 
 ////////// TYPE DEFINITIONS //////////
+
+/**
+ * DHCP opcode
+ */
+typedef enum {
+    BOOTREQUEST = 1,
+    BOOTREPLY = 2
+} dhcp_opcode;
+
+typedef enum {
+    PAD = 0,
+    END = 255
+} dhcp_option_code;
 
 /**
  * DHCP Message
  */
 typedef struct dhcp_message {
-    uint8_t op;
-    uint8_t htype;
-    uint8_t hlen;
-    uint8_t hops;
-    uint32_t xid;
-    uint16_t secs;
-    uint16_t flags;
-    uint32_t ciaddr;
-    uint32_t yiaddr;
-    uint32_t siaddr;
-    uint32_t giaddr;
-    uint8_t chaddr[16];
-    uint8_t sname[64];
-    uint8_t file[128];
-    uint32_t magic_cookie;
-    uint8_t options[308];
+    uint8_t op;            // DHCP opcode
+    uint8_t htype;         // Hardware address type
+    uint8_t hlen;          // Hardware address length
+    uint8_t hops;          // Number of hops
+    uint32_t xid;          // Transaction ID
+    uint16_t secs;         // Seconds elapsed since client began address acquisition or renewal process
+    uint16_t flags;        // DHCP flags
+    uint32_t ciaddr;       // Client IP address
+    uint32_t yiaddr;       // Your (client) IP address
+    uint32_t siaddr;       // Next server IP address
+    uint32_t giaddr;       // Relay agent IP address
+    uint8_t chaddr[16];    // Client hardware address
+    uint8_t sname[64];     // Optional server host name
+    uint8_t file[128];     // Boot file name
+    dhcp_options options;  // DHCP options
 } dhcp_message;
 
+/**
+ * DHCP Option
+ */
+typedef struct dhcp_option {
+    uint8_t code;
+    uint8_t length;
+    uint8_t *value;
+} dhcp_option;
+
+/**
+ * DHCP Options
+ */
+typedef struct dhcp_options {
+    uint8_t count;         // Number of options
+    dhcp_option *options;  // List of options
+} dhcp_options;
 
 
 ////////// FUNCTIONS //////////
 
 ///// PARSING /////
 
+/**
+ * @brief Parse the header of a DHCP message (not including options)
+ * 
+ * @param data a pointer to the start of the DHCP message
+ * @return the parsed DHCP message with the header fields filled in
+ */
+dhcp_message dhcp_parse_header(uint8_t *data);
 
+/**
+ * @brief Parse a DHCP option
+ * 
+ * @param data a pointer to the start of the DHCP option
+ * @return the parsed DHCP option
+ */
+dhcp_option dhcp_parse_option(uint8_t *data);
+
+/**
+ * @brief Parse DHCP options
+ * 
+ * @param data a pointer to the start of the DHCP options list
+ * @return a pointer to the start of the parsed DHCP options
+ */
+dhcp_options dhcp_parse_options(uint8_t *data);
+
+/**
+ * @brief Parse a DHCP message
+ * 
+ * @param data a pointer to the start of the DHCP message
+ * @return the parsed DHCP message
+ */
+dhcp_message dhcp_parse_message(uint8_t *data);
 
 
 ///// PRINTING /////
 
+/**
+ * @brief Print a DHCP message
+ * 
+ * @param message the DHCP message to print
+ */
+void dhcp_print_message(dhcp_message message);
+
+/**
+ * @brief Print a DHCP option
+ * 
+ * @param option the DHCP option to print
+ */
+void dhcp_print_option(dhcp_option option);
+
+/**
+ * @brief Print DHCP options
+ * 
+ * @param options the DHCP options to print
+ */
+void dhcp_print_options(dhcp_options options);
 
 
 #endif /* _IOTFIREWALL_DHCP_ */
--- src/parsers/dhcp.c
+++ src/parsers/dhcp.c
@@ -10,3 +10,111 @@
 
 #include "parsers/dhcp.h"
 
+///// PARSING /////
+
+/**
+ * @brief Parse a DHCP message
+ * 
+ * @param data a pointer to the start of the DHCP message
+ * @return the parsed DHCP message
+ */
+dhcp_message dhcp_parse_message(uint8_t *data) {
+    // Parse constant fields
+    dhcp_message message = dhcp_parse_header(data);
+    // Parse DHCP options
+    message.options = dhcp_parse_options(data);
+    // Return
+    return message;
+}
+
+/**
+ * @brief Parse the header of a DHCP message (not including options)
+ * 
+ * @param data a pointer to the start of the DHCP message
+ * @return the parsed DHCP message with the header fields filled in
+ */
+dhcp_message dhcp_parse_header(uint8_t *data) {
+    dhcp_message message;
+    // Opcode: 1 byte
+    message.op = *data;
+    // htype: 1 byte
+    message.htype = *(data + 1);
+    // hlen: 1 byte
+    message.hlen = *(data + 2);
+    // hops: 1 byte
+    message.hops = *(data + 3);
+    // xid: 4 bytes
+    message.xid = ntohl(*((uint32_t *) (data + 4)));
+    // secs: 2 bytes
+    message.secs = ntohs(*((uint16_t *) (data + 8)));
+    // flags: 2 bytes
+    message.flags = ntohs(*((uint16_t *) (data + 10)));
+    // ciaddr: 4 bytes
+    message.ciaddr = ntohl(*((uint32_t *) (data + 12)));
+    // yiaddr: 4 bytes
+    message.yiaddr = ntohl(*((uint32_t *) (data + 16)));
+    // siaddr: 4 bytes
+    message.siaddr = ntohl(*((uint32_t *) (data + 20)));
+    // giaddr: 4 bytes
+    message.giaddr = ntohl(*((uint32_t *) (data + 24)));
+    // chaddr: 16 bytes
+    memcpy(message.chaddr, data + 28, sizeof(uint8_t) * 16);
+    // sname: 64 bytes
+    memcpy(message.sname, data + 44, sizeof(uint8_t) * 64);
+    // file: 128 bytes
+    memcpy(message.file, data + 108, sizeof(uint8_t) * 128);
+    // DHCP options
+    message.options = dhcp_parse_options(data + DHCP_HEADER_SIZE);
+    return message;
+}
+
+/**
+ * @brief Parse DHCP options
+ * 
+ * @param data a pointer to the start of the DHCP options list
+ * @return a pointer to the start of the parsed DHCP options
+ * 
+ * TODO: realloc if too many options
+ */
+dhcp_options dhcp_parse_options(uint8_t *data) {
+    // Init
+    dhcp_options options;
+    options.count = 0;
+    options.options = (dhcp_option *) malloc(sizeof(dhcp_option) * DHCP_MAX_OPTION_COUNT);
+    // Parse options
+}
+
+/**
+ * @brief Parse a DHCP option
+ * 
+ * @param data a pointer to the start of the DHCP option
+ * @return the parsed DHCP option
+ */
+dhcp_option dhcp_parse_option(uint8_t *data) {
+    dhcp_option option;
+    option.code = *data;
+    if (option.code == PAD || option.code == END) {
+        option.length = 0;
+        option.value = NULL;
+    } else {
+        option.length = *(data + 1);
+        option.value = data + 2;
+    }
+}
+
+
+///// PRINTING /////
+
+/**
+ * @brief Print a DHCP message
+ * 
+ * @param message the DHCP message to print
+ */
+void dhcp_print_message(dhcp_message message);
+
+/**
+ * @brief Print a DHCP option
+ * 
+ * @param option the DHCP option to print
+ */
+void dhcp_print_option(dhcp_option option);
