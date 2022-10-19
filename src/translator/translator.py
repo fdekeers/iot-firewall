01ffdeb26d69d4fc40787c11d142de659f6cf629
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2022-10-19 09:30:36 +0200

    Refactor complete

--- src/translator/translator.py
+++ src/translator/translator.py
@@ -58,22 +58,31 @@ if __name__ == "__main__":
         # Loop over the device's individual policies
         if "individual-policies" in profile:
             for policy_name in profile["individual-policies"]:
+                profile_data = profile["individual-policies"][policy_name]
                 # Populate Jinja2 templates with general data for single policies
-                policy_jinja = policy_name.replace("-", "_")
                 states = ["STATE_0", "STATE_1"]
-                header_dict["policy"] = policy_jinja
+                header_dict["policy"] = policy_name
                 header_dict["max_threads"] = 1
                 header_dict["states"] = states
                 header_dict["nfq_id_base"] = nfq_id_base
-                callback_dict = {"policy": policy_jinja, "multithread": False, "states": states, "current_state": 0}
-                main_dict = {"policy": policy_jinja, "multithread": False, "nfq_id_offset": 0}
+                callback_dict = {
+                    "policy": policy_name,
+                    "multithread": False,
+                    "states": states,
+                    "current_state": 0,
+                    "direction": profile_data["direction"]
+                }
+                main_dict = {
+                    "policy": policy_name,
+                    "multithread": False,
+                    "nfq_id_offset": 0
+                }
 
                 # Create nftables chain for this policy
                 command = f"sudo nft add chain {nft_table} {policy_name} {{ type filter hook ingress device enp0s8 priority 0\; policy drop \; }}"
                 subprocess.run(command, shell=True)
 
                 # Create policy and parse it
-                profile_data = profile["individual-policies"][policy_name]
                 policy = Policy(policy_name, profile_data, device)
                 accumulators = policy.parse()
 
@@ -94,14 +103,14 @@ if __name__ == "__main__":
                 nfq_id_base += 10
 
                 # Retrieve Jinja2 template directories
-                header_dict = {**header_dict,
-                    "custom_parsers": [accumulators["custom_parser"]] if "custom_parser" in accumulators else []
+                custom_parsers = {policy_name: accumulators["custom_parser"]} if "custom_parser" in accumulators else {}
+                header_dict = {
+                    **header_dict,
+                    "custom_parsers": set(custom_parsers.values())
                 }
                 callback_dict = {
                     **callback_dict,
-                    "custom_parsers": [accumulators["custom_parser"]] if "custom_parser" in accumulators else [],
-                    "current_state": 0,
-                    "direction": profile_data["direction"],
+                    "custom_parsers": custom_parsers,
                     "nfq": accumulators["nfq"]
                 }
 
@@ -122,46 +131,45 @@ if __name__ == "__main__":
             for interaction_policy_name in profile["interaction-policies"]:
                 interaction_policy = profile["interaction-policies"][interaction_policy_name]
                 # Populate Jinja2 templates with general data for interaction policies
-                policy_jinja = interaction_policy_name.replace("-", "_")
-                header_dict["policy"] = policy_jinja
+                header_dict["policy"] = policy_name
                 header_dict["max_threads"] = len(interaction_policy)
                 multithread = len(interaction_policy) > 1
                 states = list(map(lambda i: f"STATE_{i}", range(len(interaction_policy))))
                 header_dict["states"] = states
                 header_dict["nfq_id_base"] = nfq_id_base
                 callback_dict = {"multithread": multithread}
-                main_dict = {"policy": policy_jinja, "multithread": multithread}
+                main_dict = {"policy": policy_name, "multithread": multithread}
 
                 # Create nftables chain for this policy
-                command = f"sudo nft add chain {nft_table} {policy_name} {{ type filter hook ingress device enp0s8 priority 0\; policy drop \; }}"
+                command = f"sudo nft add chain {nft_table} {interaction_policy_name} {{ type filter hook ingress device enp0s8 priority 0\; policy drop \; }}"
                 subprocess.run(command, shell=True)
 
                 # Iterate on single policies
                 current_state = 0
-                custom_parsers = []
+                custom_parsers = {}
                 policies = []
                 callback_funcs = ""
                 for single_policy_name in interaction_policy:
                     # Create policy and parse it
-                    single_policy_jinja = single_policy_name.replace("-", "_")
-                    policies.append(single_policy_jinja)
+                    policies.append(single_policy_name)
                     profile_data = interaction_policy[single_policy_name]
                     single_policy = Policy(single_policy_name, profile_data, device)
                     accumulators = single_policy.parse()
 
                     # Update high-level accumulators
                     if "custom_parser" in accumulators:
-                        custom_parsers.append(accumulators["custom_parser"])
+                        custom_parsers[single_policy_name] = accumulators["custom_parser"]
 
                     # Add nftables rules
-                    nft_rule_forward = f"sudo nft add rule {nft_table} {policy_name}"
+                    rule_base = f"sudo nft add rule {nft_table} {interaction_policy_name}"
+                    nft_rule_forward = rule_base
                     nft_rule_backward = ""
                     nft_matches = accumulators["nft"]
                     for i in range(len(nft_matches)):
                         nft_rule_forward += f" {nft_matches[i]['forward']}"
                         # Add backward rule (if necessary)
                         if "backward" in nft_matches[i]:
-                            nft_rule_backward = nft_rule_backward + f" {nft_matches[i]['backward']}" if nft_rule_backward else f"sudo nft add rule {nft_table} {policy_name} {nft_matches[i]['backward']}"
+                            nft_rule_backward = nft_rule_backward + f" {nft_matches[i]['backward']}" if nft_rule_backward else f"{rule_base} {nft_matches[i]['backward']}"
                     nft_rule_forward += f" queue num {nfq_id_base + current_state}"
                     subprocess.run(nft_rule_forward, shell=True)
                     if nft_rule_backward:
@@ -169,13 +177,23 @@ if __name__ == "__main__":
                         subprocess.run(nft_rule_backward, shell=True)
 
                     # Add callback function for this single policy
-                    callback_dict = {**callback_dict, "policy": single_policy_jinja, "custom_parsers": custom_parsers, "states": states, "current_state": current_state, "direction": profile_data["direction"], "nfq": accumulators["nfq"]}
+                    callback_dict = {
+                        **callback_dict,
+                        "policy": single_policy_name,
+                        "custom_parsers": custom_parsers,
+                        "states": states,
+                        "current_state": current_state,
+                        "direction": profile_data["direction"],
+                        "nfq": accumulators["nfq"]}
                     callback_funcs += env.get_template("callback.c.j2").render(callback_dict)
 
                     current_state += 1
                 
                 # Render Jinja2 templates
-                header_dict = {**header_dict, "custom_parsers": set(custom_parsers)}
+                header_dict = {
+                    **header_dict,
+                    "custom_parsers": set(custom_parsers.values())
+                }
                 header = env.get_template("header.c.j2").render(header_dict)
                 main_dict = {**main_dict, "policies": policies}
                 main = env.get_template("main.c.j2").render(main_dict)
