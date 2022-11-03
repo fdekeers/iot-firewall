ae07dc5e31ce0e1eb947f856955959e5943756c5
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2022-11-03 09:29:54 +0100

    Unit test for nft_counter

--- include/nft_counter.h
+++ include/nft_counter.h
@@ -20,18 +20,20 @@
 /**
  * @brief Read the packet count value of an nftables counter.
  * 
+ * @param table_name name of the nftables table containing the counter
  * @param counter_name name of the nftables counter to read
  * @return packet count value of the counter
  */
-uint32_t counter_read_packets(char* counter_name);
+uint32_t counter_read_packets(char *table_name, char *counter_name);
 
 /**
  * @brief Read the bytes value of an nftables counter.
  *
+ * @param table_name name of the nftables table containing the counter
  * @param counter_name name of the nftables counter to read
  * @return bytes value of the counter
  */
-uint32_t counter_read_bytes(char *counter_name);
+uint32_t counter_read_bytes(char* table_name, char *counter_name);
 
 
 #endif
--- src/nft_counter.c
+++ src/nft_counter.c
@@ -13,15 +13,16 @@
 /**
  * @brief Generic function to read an nftables counter value.
  *
+ * @param table_name name of the nftables table containing the counter
  * @param counter_name name of the nftables counter to read
  * @param num number of the value to read (1 for packets, 2 for bytes)
  * @return value read from the counter
  */
-static uint32_t counter_read(char *counter_name, uint8_t num) {
+static uint32_t counter_read(char *table_name, char *counter_name, uint8_t num) {
     // Build command
-    uint16_t length = 52 + strlen(counter_name);
+    uint16_t length = 58 + strlen(table_name) + strlen(counter_name);
     char cmd[length];
-    int ret = snprintf(cmd, length, "nft list counter %s | grep packets | awk '{print $%hhu}'", counter_name, num * 2);
+    int ret = snprintf(cmd, length, "sudo nft list counter %s %s | grep packets | awk '{print $%hhu}'", table_name, counter_name, num * 2);
     if (ret != length - 1)
     {
         fprintf(stderr, "Error while building command to read counter %s\n", counter_name);
@@ -49,19 +50,21 @@ static uint32_t counter_read(char *counter_name, uint8_t num) {
 /**
  * @brief Read the packet count value of an nftables counter.
  *
+ * @param table_name name of the nftables table containing the counter
  * @param counter_name name of the nftables counter to read
  * @return packet count value of the counter
  */
-uint32_t counter_read_packets(char *counter_name) {
-    return counter_read(counter_name, 1);
+uint32_t counter_read_packets(char *table_name, char *counter_name) {
+    return counter_read(table_name, counter_name, 1);
 }
 
 /**
  * @brief Read the bytes value of an nftables counter.
  *
+ * @param table_name name of the nftables table containing the counter
  * @param counter_name name of the nftables counter to read
  * @return bytes value of the counter
  */
-uint32_t counter_read_bytes(char *counter_name) {
-    return counter_read(counter_name, 2);
+uint32_t counter_read_bytes(char *table_name, char *counter_name) {
+    return counter_read(table_name, counter_name, 2);
 }
