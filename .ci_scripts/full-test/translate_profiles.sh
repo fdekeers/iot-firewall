7ccbba7be9f5901fd62d317fd12b94d0cd7f0d8f
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2022-11-24 18:36:35 +0100

    Fixed Github CI script

--- .ci_scripts/full-test/translate_profiles.sh
+++ .ci_scripts/full-test/translate_profiles.sh
@@ -1,4 +1,4 @@
 for DEVICE in $GITHUB_WORKSPACE/devices/*
 do
-    sudo python3 $GITHUB_WORKSPACE/src/translator/translator.py $GITHUB_WORKSPACE/devices/$DEVICE/profile.yaml
+    sudo python3 $GITHUB_WORKSPACE/src/translator/translator.py $DEVICE/profile.yaml
 done
