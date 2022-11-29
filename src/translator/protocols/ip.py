3e9e818fd0f63a152e046982186c2d0c59032dd9
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2022-11-29 11:36:39 +0100

    Refactor: dispatch IPv4 and IPv6

---
+++ src/translator/protocols/ip.py
@@ -0,0 +1,56 @@
+from protocols.Protocol import Protocol
+
+class ip(Protocol):
+
+    # Class variables
+    layer = 3               # Protocol OSI layer
+    custom_parser = False   # Whether the protocol has a custom parser
+    
+    # Supported keys in YAML profile
+    supported_keys = [
+        "src",
+        "dst"
+    ]
+
+
+    def parse(self, direction: str = "out", initiator: str = "src") -> dict:
+        """
+        Parse the IP (v4 or v6) protocol.
+
+        Args:
+            direction (str): Direction of the traffic (in, out, or both).
+            initiator (str): Initiator of the connection (src or dst).
+        Returns:
+            dict: Dictionary containing the (forward and backward) nftables and nfqueue rules for this policy.
+        """
+        # Lambda function to explicit a self or a well-known IP address
+        func = lambda ip: self.device[self.protocol_name] if ip == "self" else ( self.addrs[ip] if ip in self.addrs else ip )
+
+        # Connection initiator is specified
+        if initiator:
+            # Template rules
+            template_rules = {
+                "src": {"forward": f"ct original {self.nft_prefix} saddr {{}}", "backward": f"ct original {self.nft_prefix} daddr {{}}"},
+                "dst": {"forward": f"ct original {self.nft_prefix} daddr {{}}", "backward": f"ct original {self.nft_prefix} saddr {{}}"}
+            }
+            if ((initiator == "src" and (direction == "out" or direction == "both")) or
+                (initiator == "dst" and direction == "in")):
+                # Connection initiator is the source device
+                self.add_field("src", template_rules["src"], direction, func)
+                self.add_field("dst", template_rules["dst"], direction, func)
+            elif ((initiator == "src" and direction == "in") or
+                  (initiator == "dst" and (direction == "out" or direction == "both"))):
+                # Connection initiator is the destination device
+                self.add_field("src", template_rules["dst"], direction, func)
+                self.add_field("dst", template_rules["src"], direction, func)
+
+        # Connection initiator is not specified
+        else:
+            # Handle IPv4 source address
+            rules = {"forward": f"{self.nft_prefix} saddr {{}}", "backward": f"{self.nft_prefix} daddr {{}}"}
+            self.add_field("src", rules, direction, func)
+            # Handle IPv4 destination address
+            rules = {"forward": f"{self.nft_prefix} daddr {{}}", "backward": f"{self.nft_prefix} saddr {{}}"}
+            self.add_field("dst", rules, direction, func)
+        
+        return self.rules
--- src/translator/protocols/ipv4.py
+++ src/translator/protocols/ipv4.py
@@ -1,18 +1,11 @@
-from protocols.Protocol import Protocol
+from protocols.ip import ip
 from protocols.igmp import igmp
 
-class ipv4(Protocol):
+class ipv4(ip):
 
     # Class variables
     protocol_name = "ipv4"  # Protocol name
-    layer = 3               # Protocol OSI layer
-    custom_parser = False   # Whether the protocol has a custom parser
-    
-    # Supported keys in YAML profile
-    supported_keys = [
-        "src",
-        "dst"
-    ]
+    nft_prefix = "ip"       # Prefix for nftables rules
 
     # Well-known addresses
     addrs = {
@@ -22,46 +15,3 @@ class ipv4(Protocol):
         "igmpv3": "224.0.0.22",
         **igmp.groups
     }
-
-
-    def parse(self, direction: str = "out", initiator: str = "src") -> dict:
-        """
-        Parse the IPv4 protocol.
-
-        Args:
-            direction (str): Direction of the traffic (in, out, or both).
-            initiator (str): Initiator of the connection (src or dst).
-        Returns:
-            dict: Dictionary containing the (forward and backward) nftables and nfqueue rules for this policy.
-        """
-        # Lambda function to explicit a self or a well-known IP address
-        func = lambda ip: self.device['ipv4'] if ip == "self" else ( self.addrs[ip] if ip in self.addrs else ip )
-
-        # Connection initiator is specified
-        if initiator:
-            # Template rules
-            template_rules = {
-                "src": {"forward": f"ct original saddr src={{}}", "backward": f"ct original daddr dst={{}}"},
-                "dst": {"forward": f"ct original daddr dst={{}}", "backward": f"ct original saddr src={{}}"}
-            }
-            if ((initiator == "src" and (direction == "out" or direction == "both")) or
-                (initiator == "dst" and direction == "in")):
-                # Connection initiator is the source device
-                self.add_field("src", template_rules["src"], direction, func)
-                self.add_field("dst", template_rules["dst"], direction, func)
-            elif ((initiator == "src" and direction == "in") or
-                  (initiator == "dst" and (direction == "out" or direction == "both"))):
-                # Connection initiator is the destination device
-                self.add_field("src", template_rules["dst"], direction, func)
-                self.add_field("dst", template_rules["src"], direction, func)
-
-        # Connection initiator is not specified
-        else:
-            # Handle IPv4 source address
-            rules = {"forward": "ip saddr {}", "backward": "ip daddr {}"}
-            self.add_field("src", rules, direction, func)
-            # Handle IPv4 destination address
-            rules = {"forward": "ip daddr {}", "backward": "ip saddr {}"}
-            self.add_field("dst", rules, direction, func)
-        
-        return self.rules
