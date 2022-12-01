8b2b5fb901aefffcee88e4867a9f697db9b82b0c
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2022-12-01 17:31:21 +0100

    Support for including template patterns

--- src/translator/yaml_loaders/IncludeLoader.py
+++ src/translator/yaml_loaders/IncludeLoader.py
@@ -31,24 +31,50 @@ def construct_include(loader: IncludeLoader, node: yaml.Node) -> dict:
     Returns:
         dict: included pattern (from this or another YAML profile)
     """
-
-    # Retrieve path of file and member to include
     scalar = loader.construct_scalar(node)
-    split = scalar.split('#')
+    
+    # Split profile and values
+    split1 = scalar.split(" ")
+    profile = split1[0]
+    values = split1[1:]
+
+    # Parse values into dictionary
+    values_dict = {}
+    for value in values:
+        split_value = value.split(":")
+        if len(split_value) == 2:
+            values_dict[split_value[0]] = split_value[1]
+
+    # Split path and pattern from profile
+    split2 = profile.split('#')
     path = os.path.abspath(loader.stream.name)  # Default path, the current profile
-    if len(split) == 1:
-        members = split[0]
-    elif len(split) == 2:
-        if split[0] != "self":
-            path = os.path.join(os.path.dirname(path), split[0])
-        members = split[1]
+    if len(split2) == 1:
+        members = split2[0]
+    elif len(split2) == 2:
+        if split2[0] != "self":
+            path = os.path.join(os.path.dirname(path), split2[0])
+        members = split2[1]
 
     # Load member to include
+    data = {}
     with open(path, 'r') as f:
         data = yaml.load(f, IgnoreLoader)
         for member in members.split('.'):
             data = data[member]
-        return data
+    
+    # Populate values
+    data_top = data
+    for key, value in values_dict.items():
+        split_key = key.split('.')
+        i = 0
+        for sub_key in split_key:
+            if i == len(split_key) - 1:
+                data[sub_key] = value
+            else:
+                data = data[sub_key]
+                i += 1
+    
+    return data_top
 
 
 # Add custom constructor
