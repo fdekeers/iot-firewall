7c4a028e1e77f35e8511f0d88cff9a04f1db0d99
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2022-12-09 17:02:14 +0100

    CI: install valgrind

--- .ci_scripts/full-test/install_packages.sh
+++ .ci_scripts/full-test/install_packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt update
-apt install gcc make cmake libcunit1 libcunit1-dev net-tools nftables libnetfilter-queue-dev
+apt install gcc make cmake libcunit1 libcunit1-dev net-tools nftables libnetfilter-queue-dev valgrind
