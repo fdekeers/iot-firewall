b8e29d4198b404a0cbfee70b54df2f97f6e4fbb1
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2022-12-12 15:46:13 +0100

    Refactor CI valgrind test

--- .ci_scripts/full-test/run_tests.sh
+++ .ci_scripts/full-test/run_tests.sh
@@ -1,4 +1,17 @@
+EXITCODE=0
+
 for file in $GITHUB_WORKSPACE/bin/test/*
 do
-    "$file"
+    if [[ $1 -eq valgrind ]]
+    then
+        valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all --error-exitcode=1 "$file"
+    else
+        "$file"
+    fi
+    if [[ $? -ne 0 ]]
+    then
+        EXITCODE=1
+    fi
 done
+
+exit $EXITCODE
--- .ci_scripts/full-test/run_valgrind.sh
+++
@@ -1,4 +0,0 @@
-for file in $GITHUB_WORKSPACE/bin/test/*
-do
-    valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all --error-exitcode=1 "$file"
-done
