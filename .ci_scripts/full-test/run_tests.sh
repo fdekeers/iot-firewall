332ce6503faf8a06bc56b5755b454d5398beff74
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2022-12-13 09:54:58 +0100

    Fixed CI script condition

--- .ci_scripts/full-test/run_tests.sh
+++ .ci_scripts/full-test/run_tests.sh
@@ -2,7 +2,7 @@ EXITCODE=0
 
 for file in $GITHUB_WORKSPACE/bin/test/*
 do
-    if [[ $1 && $1 -eq valgrind ]]
+    if [[ $# -eq 1 && $1 == valgrind ]]
     then
         valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all --error-exitcode=1 "$file"
     else
