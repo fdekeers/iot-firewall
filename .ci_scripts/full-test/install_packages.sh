3e4b242e2fbdea9a9fa248916104d4192d22e8eb
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2022-12-13 11:04:27 +0100

    Install cppcheck on Github Actions

--- .ci_scripts/full-test/install_packages.sh
+++ .ci_scripts/full-test/install_packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt update
-apt install gcc make cmake libcunit1 libcunit1-dev net-tools nftables libnetfilter-queue-dev valgrind
+apt install -y gcc make cmake libcunit1 libcunit1-dev net-tools nftables libnetfilter-queue-dev valgrind cppcheck
